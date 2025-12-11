271f7dbaecacdf85f4ba5cfcb0dc1695b1e31ea1
fleXXo22 <hendrik.borchert@icloud.com>
2025-12-11 10:44:07 +0100

    v2

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ i2c = machine.I2C(1, I2C_scl, I2C_sda)
 #Scale
 SPI_clk = machine.Pin(4, Pin.IN, pull=machine.Pin.PULL_DOWN)
 SPI_data = machine.Pin(16, machine.Pin.OUT)
+SCALE_SCALE = 425 #adjust to fit your scale
 
 #general info
 #User Settings
@@ -44,6 +45,7 @@ PASSWORD = "Password"
 #initialize variables
 wlan = nw.WLAN()
 pn532 = PN532_I2C(i2c, debug=False)
+Scale = hx711.HX711(SPI_clk, SPI_data)
 
 
 def interrupt():
@@ -57,13 +59,15 @@ def connect_to_wifi():
 def mode_updateWeight():
     nfc_id = get_NFC_id()
     if nfc_id != False:
-
+        spool_id = get_Spool_id(nfc_id)
+        weight = get_weight()
         pass
 
 
 
 def get_NFC_id():
     try:
+        Scale.tare()
         nfc_id = pn532.read_passive_target(timeout=READ_NFC_TIMEOUT) #read the NFC ID
         if nfc_id:
             print("Found NFC tag with UID:", [hex(i) for i in nfc_id])
@@ -95,11 +99,30 @@ def main_loop():
             pass
 
 
+def get_weight():
+
+    new_value = 10000
+    old_value = 20000
+    while (new_value/old_value < 0.999) or (new_value/old_value > 1.001):
+        old_value = new_value
+        new_value = Scale.get_units()
+
+    return new_value
+
+def prepare_scale():
+    Scale.set_scale(SCALE_SCALE)
+    time.sleep(5)
+    Scale.tare(5)
+
 ## Spoolman functions
 
 def do_API_get_call(argument):
     api_call = f"{SURL}{argument}"
     response = requests.get(api_call)
+    return response.json()
+def do_API_patch_call(argument):
+    api_call = f"{SURL}{argument}"
+    response = requests.patch(api_call)
     return response.json()
     
 
@@ -107,10 +130,15 @@ def get_Spool_id(nfc_id):
     self.nfc_id = nfc_id
     spool_id = do_API_call(f"/spool?lot_nr={nfc_id}")
 
+def set_Spool_weight(spool_id, weight):
+   pass 
+
+
 #machine interrupts
 Button1.irq(trigger=machine.Pin.IRQ_Falling, handler=interrupt)
 Button2.irq(trigger=machine.Pin.IRQ_Falling, handler=interrupt)
 
 
 pn532.SAM_configuration()
+prepare_scale()
 main_loop()
